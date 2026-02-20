ac18bfadd291ed3f49f8c1233b4412d5dc34478f
Jhon Joshua Abutan <sjosh2503@gmail.com>
2026-02-20 23:43:31 +0800

    Update PrivacyPolicy.tsx

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -7,6 +7,7 @@ const PrivacyPolicy: React.FC = () => {
       <p>Effective Date: February 20, 2026</p>
       <section className="mt-6">
         <h2 className="text-xl font-semibold">1. Data Collection</h2>
+        
         <p>We only collect your name and email address via Google/Facebook login to create your account.</p>
       </section>
       <section className="mt-6">
