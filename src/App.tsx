6b0e4efcae23143c5149071062ec18153794e584
Jhon Joshua Abutan <sjosh2503@gmail.com>
2026-02-20 23:56:10 +0800

    Add PrivacyPolicy page import to App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import AuthPage from "./components/AuthPage"; 
+import PrivacyPolicy from "./pages/PrivacyPolicy"; 
 import Profile from "./pages/Profile"; // <--- ADDED THIS
 
 const queryClient = new QueryClient();
