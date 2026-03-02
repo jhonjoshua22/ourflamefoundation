a91a1ebe23c5cc1799f663a9980c7faf7c1b0ede
Jhon Joshua Abutan <sjosh2503@gmail.com>
2026-03-03 00:32:08 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,5 @@
+my current one
+
 import HeroSection from "@/components/HeroSection";
 import ImpactSection from "@/components/ImpactSection";
 import Testimonials from "@/components/Testimonials";
@@ -10,53 +12,32 @@ import PartnerSection from "@/components/PartnerSection";
 import NewsEventsSection from "@/components/NewsEventsSection";
 
 const Index = () => {
-  return (
-    <div className="flex flex-col">
-      {/* 1. HOME */}
-      <section id="home">
-        <HeroSection />
-      </section>
-      
-      {/* 2. ABOUT */}
-      <section id="about">
-        <AboutUsSection />
-      </section>
-
-      {/* 3. FLAME GAME */}
-      <section id="flame-game">
-        <FlameGame />
-        <Dashboard />
-      </section>
-      
-      {/* 4. SERVICES */}
-      <section id="services">
-        <ImpactSection />
-      </section>
-
-      {/* 5. PRESENCE */}
-      <section id="presence">
-        <GlobalMap />
-      </section>
-
-      {/* 6. NEWS */}
-      <section id="news">
-        <NewsEventsSection />
-      </section>
-
-      {/* 7. PROCESS */}
-      <section id="process">
-        <ProcessSection />
-      </section>
-
-      {/* 8. IMPACT */}
-      <section id="impact">
-        <Testimonials />
-      </section>
-      
-      {/* Partnership Ecosystem (No Nav Link) */}
-      <PartnerSection />
-    </div>
-  );
+  return (
+    <div className="flex flex-col">
+      {/* 
+          This page renders inside the <Outlet /> of MainLayout. 
+      */}
+      
+      <HeroSection />
+      
+      {/* The Flame Game & Dashboard Sections */}
+      <FlameGame />
+      <Dashboard />
+      
+      {/* Infrastructure & Social Proof */}
+      <GlobalMap />
+      <ProcessSection />
+      <Testimonials />
+      
+      {/* Foundation Details */}
+      <AboutUsSection />
+      <NewsEventsSection />
+      <ImpactSection />
+      
+      {/* Partnership Ecosystem */}
+      <PartnerSection />
+    </div>
+  );
 };
 
 export default Index;
