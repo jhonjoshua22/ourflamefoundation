0a92680dc857a50d7f5b4665b8d8f878d59d22d6
Jhon Joshua Abutan <sjosh2503@gmail.com>
2026-03-15 01:10:18 +0800

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,15 +56,15 @@ const Navbar = () => {
     { name: "Flame Game", href: "/#flame-game" },
     { name: "Ranks", href: "/#tiers" },
     { name: "Our Games", href: "/#gallery" },
-    { name: "About", href: "/#home" },
+    { name: "Rewards", href: "/scoretable" },
     { name: "Growth", href: "/#presence" },
+    { name: "Reviews", href: "/#impact" },
     { name: "Products", href: "/#products" },
     { name: "Resources", href: "/#resources" },
     { name: "Events", href: "/#news" },
     { name: "New", href: "/#services" },
     { name: "Support", href: "/#people" },
-    { name: "Reviews", href: "/#impact" },
-    { name: "Rewards", href: "/scoretable" },
+    { name: "About", href: "/#home" },
     { name: "Contacts", href: "/#contacts" },
   ];
 
