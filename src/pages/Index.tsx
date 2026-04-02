a05c9f5701849424b809ab48b616f6cd1e9df733
Jhon Joshua Abutan <sjosh2503@gmail.com>
2026-04-02 22:27:16 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/HeroSection";
 import ImpactSection from "@/components/ImpactSection";
 import Testimonials from "@/components/Testimonials";
+import ProcessSection from "@/components/ProcessSection";
 import AboutUsSection from "@/components/AboutUsSection";
 import FlameGame from "@/components/FlameGame";
 import Dashboard from "@/components/Dashboard";
@@ -14,20 +15,24 @@ const Index = () => {
     <div className="flex flex-col">
       
       
+      {/* The Flame Game & Dashboard Sections */}
       <FlameGame />
       <Dashboard />
       <GameGallery />
       
+      {/* Infrastructure & Social Proof */}
       <GlobalMap />
       <Testimonials />
       <AboutUsSection />
       
+      {/* Foundation Details */}
       <NewsEventsSection />
-      
+      <ImpactSection />
       
+      {/* Partnership Ecosystem */}
       <PartnerSection />
       <HeroSection />
-      <ImpactSection />
+      <ProcessSection />
     </div>
   );
 };
