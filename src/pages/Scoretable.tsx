65299ab5ab6fe11388c143f92b665a00eb0c0eb9
Jhon Joshua Abutan <sjosh2503@gmail.com>
2026-05-01 21:31:49 +0800

    Update Scoretable.tsx

--- src/pages/Scoretable.tsx
+++ src/pages/Scoretable.tsx
@@ -522,11 +522,11 @@ const Scoretable = () => {
                       <tr className="bg-orange-600/10 border-b border-orange-600/20 text-white">
                         <td className="p-5 font-black text-orange-500">{stats.totalMembers.toLocaleString()}</td>
                         <td className="p-5 font-black text-orange-500">{stats.totalReferred.toLocaleString()}</td>
-                        <td className="p-5 font-black text-orange-500">${stats.totalInvested.toLocaleString()}</td>
-                        <td className="p-5 font-black text-zinc-500">—</td>
+                        <td className="p-5 font-black text-orange-500">$300K</td>
+                        <td className="p-5 font-black text-zinc-500">4</td>
                         <td className="p-5 font-black text-orange-500">{stats.totalFollowers.toLocaleString()}</td>
                         <td className="p-5 text-blue-400 font-mono font-bold text-sm">100M+</td>
-                        <td className="p-5 text-right text-purple-400 font-black italic">$100M</td>
+                        <td className="p-5 text-right text-purple-400 font-black italic">$1.9B</td>
                       </tr>
                     </thead>
                     <tbody className="divide-y divide-zinc-800">
